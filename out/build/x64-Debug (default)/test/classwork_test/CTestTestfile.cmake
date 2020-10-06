29739f31bddf8d7a049d01a933a80c37dc5087aa
ac-bdwlf-86 <adam.dominguez2@g.austincc.edu>
2020-10-05 22:15:33 -0500

    Testing using both PCs

--- out/build/x64-Debug (default)/test/classwork_test/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: C:/Users/Chris/source/repos/acc-cosc-1337-fall-2020/acc-cosc-1337-fall-2020-ac-bdwlf-86/test/classwork_test
-# Build directory: C:/Users/Chris/source/repos/acc-cosc-1337-fall-2020/acc-cosc-1337-fall-2020-ac-bdwlf-86/out/build/x64-Debug (default)/test/classwork_test
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("04_assign_test")
--- out/build/x64-Debug (default)/test/homework_test/03_decisions_test/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: C:/Users/Chris/source/repos/acc-cosc-1337-fall-2020/acc-cosc-1337-fall-2020-ac-bdwlf-86/test/homework_test/03_decisions_test
-# Build directory: C:/Users/Chris/source/repos/acc-cosc-1337-fall-2020/acc-cosc-1337-fall-2020-ac-bdwlf-86/out/build/x64-Debug (default)/test/homework_test/03_decisions_test
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-add_test(hw_03_tests "C:/Users/Chris/source/repos/acc-cosc-1337-fall-2020/acc-cosc-1337-fall-2020-ac-bdwlf-86/out/build/x64-Debug (default)/test/homework_test/03_decisions_test/hw_03_tests.exe" "-s")
-set_tests_properties(hw_03_tests PROPERTIES  _BACKTRACE_TRIPLES "C:/Users/Chris/source/repos/acc-cosc-1337-fall-2020/acc-cosc-1337-fall-2020-ac-bdwlf-86/test/homework_test/03_decisions_test/CMakeLists.txt;3;add_test;C:/Users/Chris/source/repos/acc-cosc-1337-fall-2020/acc-cosc-1337-fall-2020-ac-bdwlf-86/test/homework_test/03_decisions_test/CMakeLists.txt;0;")
